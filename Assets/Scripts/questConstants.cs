7bcfc35c32cce9cb6cc238d5fbe8c0fb0e992cf0
kamisch <chengtwv2@gmail.com>
2019-04-07 17:23:36 -0400

    lol

---
+++ Assets/Scripts/questConstants.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class questConstants
+{
+    public static int questTime;
+    public static string questType;
+
+    public static string charClass;
+
+    public static void getQuestTime(int value){
+        questTime = value;
+    }
+
+    public static void getQuestType(string value){
+        questType = value;
+    }
+
+    public static void getCharClass(string value){
+        charClass = value;
+    }
+
+
+}
