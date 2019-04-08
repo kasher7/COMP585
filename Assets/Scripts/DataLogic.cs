20993183a90f6078123e55e7e04f86bb929fd58d
unknown <kyleasher@gmail.com>
2019-04-07 21:20:51 -0400

    made some loading and saving scripts

---
+++ Assets/Scripts/DataLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataLogic : MonoBehaviour
+{
+    public void SaveGameData(DataObject myObject)
+    {
+        string dataAsJson = JsonUtility.ToJson(myObject);
+        string filePath = Application.dataPath + "/Data/Data.json";
+        Debug.Log(filePath);
+        File.WriteAllText(filePath, dataAsJson);
+    }
+
+    public void LoadGameData(DataObject myObject)
+    {
+        string filePath = Application.dataPath + "/Data/Data.json";
+        string dataAsJson = File.ReadAllText(filePath);
+        myObject = JsonUtility.FromJson<DataObject>(dataAsJson);
+    }
+    
+}
