e1c0c582caed7b7e469991034f766099069fe5b5
unknown <kyleasher@gmail.com>
2019-04-08 14:45:10 -0400

    updated some date tracking logic

--- Assets/Scripts/DataObject.cs
+++ Assets/Scripts/DataObject.cs
@@ -13,22 +13,36 @@ public class DataObject
     private int questLength; //total quest length in minutes
     [SerializeField]
     private int dayCounter;
+    /*
+     NOTE: All Dates are stored as string arrays ordered as follows:
+     -[0] Month (string number)
+     -[1] Day (string number)
+     -[2] Year (string number)
+     -[3] Hour (string number)
+     -[4] Minute (string number)
+     -[5] Second (string number)
+     -[6] Period (string "am" or "pm")
+     */
+
+
     [SerializeField]
-    private double startDate;
+    private string[] startDate;
     [SerializeField]
-    private double currentDate;
+    private string[] currentDate;
+    [SerializeField]
+    private string[] lastDayPlayed;
     [SerializeField]
     private double[] dailyProgress;
     [SerializeField]
-    private double totalEXP;
+    private int totalEXP;
     [SerializeField]
     private int playerLevel;
     [SerializeField]
-    private double strengthEXP;
+    private int strengthEXP;
     [SerializeField]
-    private double charismaEXP;
+    private int charismaEXP;
     [SerializeField]
-    private double intellectEXP;
+    private int intellectEXP;
     [SerializeField]
     private int strengthLevel;
     [SerializeField]
@@ -60,14 +74,14 @@ public class DataObject
     public string PlayerName { get => playerName; set => playerName = value; }
     public int QuestLength { get => questLength; set => questLength = value; }
     public int DayCounter { get => dayCounter; set => dayCounter = value; }
-    public double StartDate { get => startDate; set => startDate = value; }
-    public double CurrentDate { get => currentDate; set => currentDate = value; }
+    public string[] StartDate { get => startDate; set => startDate = value; }
+    public string[] CurrentDate { get => currentDate; set => currentDate = value; }
     public double[] DailyProgress { get => dailyProgress; set => dailyProgress = value; }
-    public double TotalEXP { get => totalEXP; set => totalEXP = value; }
+    public int TotalEXP { get => totalEXP; set => totalEXP = value; }
     public int PlayerLevel { get => playerLevel; set => playerLevel = value; }
-    public double StrengthEXP { get => strengthEXP; set => strengthEXP = value; }
-    public double CharismaEXP { get => charismaEXP; set => charismaEXP = value; }
-    public double IntellectEXP { get => intellectEXP; set => intellectEXP = value; }
+    public int StrengthEXP { get => strengthEXP; set => strengthEXP = value; }
+    public int CharismaEXP { get => charismaEXP; set => charismaEXP = value; }
+    public int IntellectEXP { get => intellectEXP; set => intellectEXP = value; }
     public int StrengthLevel { get => strengthLevel; set => strengthLevel = value; }
     public int CharismaLevel { get => charismaLevel; set => charismaLevel = value; }
     public int IntellectLevel { get => intellectLevel; set => intellectLevel = value; }
@@ -77,4 +91,5 @@ public class DataObject
     public string[] CharismaQuestLine { get => charismaQuestLine; set => charismaQuestLine = value; }
     public string[] IntellectQuestLine { get => intellectQuestLine; set => intellectQuestLine = value; }
     public string[] FailureQuestLine { get => failureQuestLine; set => failureQuestLine = value; }
+    public string[] LastDayPlayed { get => lastDayPlayed; set => lastDayPlayed = value; }
 }
