c684caff079361fa0ce5b44eb319167400334b3b
kamisch <chengtwv2@gmail.com>
2019-04-20 16:04:53 -0400

    lalalalala

--- Assets/Scripts/questStatus.cs
+++ Assets/Scripts/questStatus.cs
@@ -10,12 +10,29 @@ public class questStatus : MonoBehaviour
     public Text type;
     public Text time;
 
+    public Text exp;
+
     public Text status;
     // Start is called before the first frame update
+
+    
     void Start()
-    {
+    {   
+        int expGained = questConstants.questTime * 20;
         type.text = "Quest Type: " + questConstants.questType;
         time.text = "Time Spent: " + questConstants.questTime;
+        exp.text =  "Exp gained" + expGained;
+        //Data update logic
+        DataObject myData = new DataObject();
+        DataLogic.LoadGameData(myData);
+        if (questConstants.questType == "Strength"){
+            myData.StrengthEXP +=expGained;
+        }else if (questConstants.questType == "Charisma"){
+            myData.CharismaEXP +=expGained;
+        } else if (questConstants.questType == "Intelligence"){
+            myData.IntellectEXP +=expGained;
+        }
+        DataLogic.SaveGameData(myData);
         status.text = "Quest completed";
     }
 
