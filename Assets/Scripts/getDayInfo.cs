e70d74627150962d0a551c96539926f402b97e0f
kamisch <chengtwv2@gmail.com>
2019-04-21 19:41:20 -0400

    completed calender days

--- Assets/Scripts/getDayInfo.cs
+++ Assets/Scripts/getDayInfo.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class getDayInfo : MonoBehaviour
 {
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i =0; i< GameObject.FindGameObjectsWithTag("calender").Length ; i++){
+            Debug.Log(GameObject.FindGameObjectsWithTag("calender")[i].GetComponentInChildren<Text>().text);
+        }
     }
 
     // Update is called once per frame
