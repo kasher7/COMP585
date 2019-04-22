f20afce30ad75b27228791aa02a9af9db293890b
kamisch <chengtwv2@gmail.com>
2019-04-21 23:01:39 -0400

    minior fix on post story

---
+++ Assets/Scripts/failedQuest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class failedQuest : MonoBehaviour
+{
+    // public Text story;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        // DataObject myData = new DataObject();
+        // DataLogic.LoadGameData(myData);
+        // if (questConstants.questType == "Strength"){
+        //     story.text = myData.PostStrengthQuestLine[myData.DayCounter];
+        //     myData.StrengthEXP +=expGained;
+        //     myData.DailyStrengthEXP[myData.DayCounter] += expGained;
+        // }else if (questConstants.questType == "Charisma"){
+        //     story.text = myData.PostCharismaQuestLine[myData.DayCounter];    
+        //     myData.CharismaEXP +=expGained;
+        //     myData.DailyCharismaEXP[myData.DayCounter] += expGained;
+        // } else if (questConstants.questType == "Intelligence"){
+        //     story.text = myData.PostIntellectQuestLine[myData.DayCounter]; 
+        //     myData.IntellectEXP +=expGained;
+        //     myData.DailyIntellectEXP[myData.DayCounter] += expGained;
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
