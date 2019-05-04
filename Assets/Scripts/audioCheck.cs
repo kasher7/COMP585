941924188f1d706555aa8086bf66510aad803b24
kamisch <chengtwv2@gmail.com>
2019-05-03 20:28:41 -0400

    added options for audio toggle

---
+++ Assets/Scripts/audioCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audio;
+    void Start()
+    {
+        audio.playOnAwake = true;
+        Debug.Log(PlayerPrefs.GetInt("audio"));
+        if (PlayerPrefs.GetInt("audio")==1){
+            audio.Play();
+        } else if (PlayerPrefs.GetInt("audio")==0){
+            audio.Stop();
+        } else{
+            audio.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/audioToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioToggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onOff(){
+        if (PlayerPrefs.GetInt("audio")==1){
+            PlayerPrefs.SetInt("audio", 0);
+        } else if (PlayerPrefs.GetInt("audio")==0){
+            PlayerPrefs.SetInt("audio", 1);
+        } else{
+            PlayerPrefs.SetInt("audio", 0);
+        }
+        
+    }
+}
